d1075a81f79f961d076262b9e6b2023812a5a923
wotj102@gmail.com <wotj102@gmail.com>
2020-09-05 22:33:35 +0900

    binary_search

---
+++ algorithm/component_search.py
@@ -0,0 +1,20 @@
+N = int(input())
+market = list(map(int, input().split()))
+
+M = int(input())
+customer= list(map(int, input().split()))
+answer =[]
+peak = 0
+
+for c in customer:
+  correct = "no"
+  peak = int(c)
+  for m in range(N):
+    if peak == market[m]:
+      correct = "yes"
+    else:
+      pass
+  answer.append(correct)
+
+for a in answer:
+  print(a, end= " ")
