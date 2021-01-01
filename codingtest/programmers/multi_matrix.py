470bd6c2b45585c22ccd8fbaa3e29a13cb152dcb
wotj102@gmail.com <wotj102@gmail.com>
2021-01-02 00:05:37 +0900

    matrix

--- codingtest/programmers/multi_matrix.py
+++ codingtest/programmers/multi_matrix.py
@@ -10,11 +10,15 @@
 
 def solution(arr1, arr2):
     answer = []
+    #arr1의 행 부분
     for i in range(len(arr1)):
         ans=[]
+        #arr2의 열 부분
         for j in range(len(arr2[0])):
             cnt = 0
+            #[열] * [행] 부분
             for k in range(len(arr2)):
+                #arr1의 [행][열] * arr2의 [행][열]
                 cnt+=arr1[i][k]*arr2[k][j]
             ans.append(cnt)
         answer.append(ans)
